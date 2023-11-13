bcbeb32478e013d04c476735055abf8290765db4
dmitrolov <dmitrolov@gmail.com>
2023-11-13 13:56:14 +0200

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <>
+            <div className="title">
+                <h1>Hello</h1>
+            </div>
+
+        </>
+    );
+};
+
+export default Index;
